1c31d93080620e189448ab80731c8f8baaa2030b
Marcos Lomboglia <marcos.lomboglia@gmail.com>
2020-04-07 15:19:58 +0100

    Add VideoPreview

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeaderNav from "./containers/HeaderNav/HeaderNav";
 import SideBar from "./containers/SideBar/SideBar";
+import Home from "./containers/Home/Home";
 
 function App() {
   return (
     <React.Fragment>
       <HeaderNav />
       <SideBar />
+      <Home />
     </React.Fragment>
   );
 }
---
+++ src/containers/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import VideoPreview from "../../components/VideoPreview/VideoPreview";
+import "./Home.scss";
+
+const home = () => {
+  return (
+    <div className="home">
+      <VideoPreview />
+    </div>
+  );
+};
+
+export default home;
