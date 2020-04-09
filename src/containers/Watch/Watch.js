25e08c822c074a96983031d7a1bddbfe128fdda1
Marcos Lomboglia <marcos.lomboglia@gmail.com>
2020-04-09 15:03:31 +0100

    Rating

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -2,13 +2,19 @@ import React from "react";
 
 import "./Watch.scss";
 import Video from "../../components/Video/Video";
-import VideoPreview from '../../components/VideoPreview/VideoPreview';
 import RelatedVideos from '../../components/RelatedVideos/RelatedVideos';
+import VideoMetadata from '../../components/VideoMetadata/VideoMetadata';
 
 const watch = () => {
   return (
-    <RelatedVideos/>
+    <div className='watch-grid'>
+        <Video className='video' id='-7fuHEEmEjs' />
+        <VideoMetadata className='metadata' viewCount={1000}/>
+        <div className='video-info-box' style={{width: '100%', height: '100px', background: '#BD10E0'}}>Video Info box</div>
+        <div className='comments' style={{width: '100%', height: '100px', background: '#9013FE'}}>comments</div>
+        <RelatedVideos className='relatedVideos'/>
+      </div>
   );
-};
+};  
 
 export default watch; 
